eef05d197d0e2794b3077f98af88c70ce71e5285
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-23 16:02:52 +0300

    required libraries scpirt added.

---
+++ source/lib/init.R
@@ -0,0 +1,6 @@
+if (!requireNamespace("ggplot2", quietly = TRUE)) {
+  print("Package ggplot2 is needed for TestR Framework. ggplot2 installing...")
+  library(ggplot2)
+}else {
+  print("ggplot2 is installed.")
+}
