8e75621a94c56bb6e611dacc57e77909be2719dc
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-23 22:39:53 +0300

    logo updated, new libraries added.

--- source/lib/init.R
+++ source/lib/init.R
@@ -1,14 +1,19 @@
-init.packages.list <- c("loadtest", "ggplot2", "ggpubr")
+init.packages.list <- c("loadtest", "ggplot2", "ggpubr", "grid", "gridExtra", "png", "jpeg", "stringr")
+current_date <- Sys.Date()
+current_time <- Sys.time()
 
 for(package in init.packages.list)
   
   if (!requireNamespace(package, quietly = TRUE)) {
-    print(paste0("Package", package, "is needed for TestR Framework.", package, "installing..."))
+    print(paste0("required package", package, "is needed for TestR Framework.", package, "installing..."))
     
     if(package == "loadtest")
       remotes::install_github("tmobile/loadtest")
+    
     else
       install.packages(package)
+    
   }else {
     print(paste0(package, " is already installed"))
+    
   }
