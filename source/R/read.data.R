5e5b938929db08facf97d197f4e10c897a3820f1
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-25 04:10:25 +0300

    read.test/config.data functions added, uyicam yatıyom by.

---
+++ source/R/read.data.R
@@ -0,0 +1,40 @@
+library(yaml)
+library(reshape2)
+
+
+
+read.test.data <- function(){
+  
+  test_path <- paste0(getwd(), "/tests/")
+  
+  df_test <- data.frame()
+  
+  test_files <- list.files(path=test_path, pattern=".yml", all.files=FALSE, full.names=FALSE)
+  
+  
+  test_list <- lapply(test_files,
+                        function(x) out <- yaml.load_file(paste0(test_path, x)))
+  
+  return(df_test <- melt(test_list))
+  
+}
+
+
+read.config.data <- function(){
+  
+  config_path <- paste0(getwd(), "/config.yml")
+  
+  df_config <- data.frame()
+  
+  config_file <- yaml.load_file(config_path)
+  
+   return(df_config <- melt(config_file))
+}
+
+var_test <- read.test.data()
+var_config <- read.config.data()
+
+
+
+
+  
